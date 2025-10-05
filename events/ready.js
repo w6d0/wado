14ce5857b2db827d8230f87574659bab66d52b21
w6d0 <wado0891@gmail.com>
2025-10-05 20:58:26 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ export default {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log(`✅ Logged in as ${client.user.tag}`);
-		client.user.setActivity('✨ Ready to serve!', { type: 3 });
+		console.log(`✅ Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity('✨ Stylish Bot is online!', { type: 3 });
 	},
 };
